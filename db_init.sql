0fcf8abbbc3d3c0ff6a3771d5de6156b4722a4bd
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2025-07-26 14:04:10 +0200

    wip

--- db_init.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER 'optatio_user'@'localhost' IDENTIFIED BY 'password';
-CREATE USER 'optatio_user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON * . * TO 'optatio_user'@'%';
-GRANT ALL PRIVILEGES ON * . * TO 'optatio_user'@'localhost';
-FLUSH PRIVILEGES;
-CREATE DATABASE optatio;
