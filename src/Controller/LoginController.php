a1048c1216fa490914a3cf2b5bf6fc05f3da6edc
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2025-10-06 19:54:48 +0200

    wip

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Content\User\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Recipient\Recipient;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkNotification;
 
 class LoginController extends AbstractController
 {
@@ -23,6 +29,40 @@ class LoginController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route('/login_check', name: 'login_check')]
+    public function check(): never
+    {
+        throw new \LogicException('This code should never be reached');
+    }
+
+    #[Route('/mlogin', name: 'app_magic_login')]
+    public function requestLoginLink(NotifierInterface $notifier, LoginLinkHandlerInterface $loginLinkHandler, UserRepository $userRepository, Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            $email = $request->getPayload()->get('email');
+
+            $user = $userRepository->findOneBy(['email' => $email]);
+
+            $loginLinkDetails = $loginLinkHandler->createLoginLink($user);
+
+            // create a notification based on the login link details
+            $notification = new LoginLinkNotification(
+                $loginLinkDetails,
+                'Optatio-Login' // email subject
+            );
+            // create a recipient for this user
+            $recipient = new Recipient($user->getEmail());
+
+            // send the notification to the user
+            $notifier->send($notification, $recipient);
+
+            // render a "Login link is sent!" page
+            return $this->render('security/login_link_sent.html.twig');
+        }
+
+        return $this->render('security/request_login_link.html.twig');
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
